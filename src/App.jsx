029c1d354a013b13f5048a76d9d5ddedbce5d17d
frako23 <frako23@gmail.com>
2023-07-12 12:56:04 -0400

    se agrega la seccion de detalles

--- src/App.jsx
+++ src/App.jsx
@@ -68,9 +68,38 @@ function App() {
 
       </header>
       {/* termina el header */}
-
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere culpa placeat, deleniti doloremque, dolores impedit animi rerum inventore id sapiente ducimus magni, accusantium excepturi incidunt consequuntur architecto! Placeat accusamus totam ducimus reprehenderit exercitationem sapiente tenetur, possimus itaque? Natus accusamus necessitatibus facere in officiis ducimus ab amet eaque nulla porro quas quae aliquid ratione earum praesentium illum cumque libero deleniti similique maxime perferendis, quam consequuntur? Deleniti, quaerat, neque deserunt alias animi aliquid enim, eveniet id doloremque pariatur doloribus tenetur inventore voluptas provident eligendi ea. Illo suscipit sapiente perspiciatis omnis iure ea cupiditate error obcaecati culpa cumque et animi dignissimos, hic unde sequi eum. Sed adipisci excepturi alias minima. Dolorem minus quas aspernatur omnis veniam rerum consectetur, maiores voluptatem ea adipisci laboriosam? Doloremque harum aliquid, molestias non nulla distinctio beatae a, quasi amet perspiciatis corporis optio voluptate inventore suscipit voluptatibus. Tempora, tenetur fugiat hic, illum obcaecati rem, cupiditate necessitatibus temporibus a eveniet architecto neque. Quae totam libero maiores, modi quaerat nisi facilis tempora vero dolor architecto ullam nemo deserunt ad! Quam at animi tenetur, numquam quia voluptate vero quae cum nemo beatae sint itaque! Nostrum velit ipsam vel assumenda nulla, laudantium, quas sed, amet ipsum vitae esse! Impedit dolor ut magni, commodi distinctio beatae! Obcaecati explicabo et tempore, nisi suscipit optio nostrum tempora omnis voluptas qui fuga doloribus assumenda unde ipsa cupiditate quos ab ipsum fugit beatae aperiam? Libero ab rem ipsam dicta commodi, delectus nihil ut incidunt soluta corrupti? Consequuntur repellat perferendis accusamus rerum assumenda ipsa consequatur, nobis est eaque perspiciatis illo culpa magnam nesciunt debitis corrupti quos. Molestias quam quas nihil, atque nostrum reprehenderit debitis fuga perspiciatis, ullam culpa distinctio facilis sunt! Illo cumque eveniet saepe dolores sequi aspernatur voluptate sapiente laboriosam corporis sit ad nisi non, distinctio cupiditate ea veniam beatae? Vitae voluptas quaerat accusantium numquam, architecto distinctio minima illum eligendi quibusdam repellendus rem eveniet iusto magnam est fugit odit. Iure, eum iusto veniam, ea, pariatur dolorem corporis et iste tempora molestiae nesciunt dolores optio temporibus odit? Dolorum esse vero explicabo voluptatibus. Nam neque quae fugit, laboriosam cumque deserunt odio! Omnis quis minima excepturi doloribus sunt numquam beatae assumenda illo, blanditiis eligendi, et vel cum saepe eaque deserunt est. Ut magni eum tempora illum impedit dignissimos sapiente, explicabo aliquam, reiciendis est ducimus, in voluptatem itaque necessitatibus fugit. Animi voluptatum magnam minima molestias dolores beatae accusamus ab repellendus nobis harum neque iusto quod, odio fuga, repudiandae possimus. Eius, mollitia? Exercitationem, aliquid quas cum architecto quibusdam excepturi, dignissimos, sequi voluptatem quis maiores optio sapiente animi? Illum eaque facere repellendus! Aut veniam sit dolorum optio inventore, modi similique iure, aliquid voluptatibus voluptas quis architecto iusto ipsam deleniti ea, nisi id repellendus nihil perferendis et ut beatae sequi placeat. Quidem officia officiis minima at ducimus alias eligendi culpa magnam magni! Repellendus maiores dolorem nobis voluptates rerum nulla asperiores unde magnam. Cupiditate maiores illum autem, sit laboriosam cum deleniti esse distinctio odit natus minus officia ipsa reiciendis dolores officiis, sed deserunt earum impedit. Velit, nostrum quasi. Sit possimus at eligendi adipisci corporis, doloremque debitis?</p>
-
+      {/* comienza la sección de detalle */}
+      <section className='detail'>
+        <div className="container">
+          <div className="row">
+            <div className="detail-item">
+              <span>
+                <i className='fas fa-tablet-alt'></i>
+              </span>
+              <h2>Diseño Responsivo</h2>
+              <div className="line"></div>
+              <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam odio et fugit assumenda alias? Accusamus, iste. Rem deserunt corporis similique ipsam sint vel reiciendis eligendi!</p>
+            </div>
+        <div className="detail-item">
+              <span>
+                <i className='fab fa-html5'></i>
+              </span>
+              <h2>HTML5</h2>
+              <div className="line"></div>
+              <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam odio et fugit assumenda alias? Accusamus, iste. Rem deserunt corporis similique ipsam sint vel reiciendis eligendi!</p>
+        </div>
+        <div className="detail-item">
+              <span>
+                <i className='far fa-bell'></i>
+              </span>
+              <h2>Soporte las 24 Horas</h2>
+              <div className="line"></div>
+              <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam odio et fugit assumenda alias? Accusamus, iste. Rem deserunt corporis similique ipsam sint vel reiciendis eligendi!</p>
+        </div>
+          </div>
+        </div>
+      </section>
+      {/* termina la sección de detalle */}
     </>
   );
 }
