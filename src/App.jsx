2cb8bfd618687a59eaeed0d704d65d81242139aa
frako23 <frako23@gmail.com>
2023-08-14 12:03:53 -0400

    se agrega el instagram de mariangel

--- src/App.jsx
+++ src/App.jsx
@@ -317,7 +317,7 @@ function App() {
                   <a href="https://www.linkedin.com/in/mariangel-rodriguez-perozo-/" className="center">
                     <i className="fa-brands fa-linkedin"></i>
                   </a>
-                  <a href="#" className="center">
+                  <a href="https://www.instagram.com/mariangellifestyle/" className="center">
                     <i className="fa-brands fa-instagram"></i>
                   </a>
                   <a href="https://wa.me/584241560579" className="center">
